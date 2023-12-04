d976d769bf54763a5d8a7beefabf45251cb439a8
196code-gray <doco827@gmail.com>
2023-12-04 14:31:52 +0900

    first commit

---
+++ src/첫날/Example1.java
@@ -0,0 +1,16 @@
+package 첫날;
+
+public class Example1 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("We are now in thread " + Thread.currentThread().getName());
+            }
+        });
+        System.out.println(Thread.currentThread().getName() + " before thread");
+        thread.start();
+        System.out.println(Thread.currentThread().getName() + " after thread");
+    }
+}
+}
